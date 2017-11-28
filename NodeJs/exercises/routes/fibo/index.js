60bb3e7dfc07001194fb2ba0f3879f471074262c
Rehan <rehansheikh15@gmail.com>
2017-11-28 16:24:29 -0600

    Cleaned github

--- NodeJs/exercises/routes/fibo/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var express = require('express');
-var fibo = require('./fibonacci');
-var router = express.Router();
-
-router.get('/:number', fibo.index);
-
-module.exports = router;
--- NodeJs/exercises/routes/routers.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-
-var routers = express.Router();
-
-routers.use("/fibonacci", require("./fibo/index"));
-
-routers.use("/bubble", require("./bubble/index"));
-
-module.exports = routers;
